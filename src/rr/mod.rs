35f1d2fba9f330ca376c0ab1018ab89a0dd55232
Sehny <sehnoutka.martin@gmail.com>
2016-12-26 16:02:29 +0100

    Test na parsovani A zaznamu

--- src/rr/mod.rs
+++ src/rr/mod.rs
@@ -4,7 +4,7 @@
 
 use std::net::Ipv4Addr;
 
-#[derive(Debug)]
+#[derive(Debug,PartialEq)]
 pub enum Rdata {
     A(Ipv4Addr),
     Generic(Vec<u8>),
--- src/wire/test.rs
+++ src/wire/test.rs
@@ -1,5 +1,9 @@
+use std::net::Ipv4Addr;
+use std::str::FromStr;
+
 use super::msg;
 use message::Header;
+use rr::Rdata;
 
 #[test]
 fn test_parse_dns_header() {
@@ -78,7 +82,7 @@ fn test_parse_query() {
     0x03, 0x6e, 0x69, 0x63, 0x02, 0x63, 0x7a, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x29,
     0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
     let q = msg::parse_dns_message(&input[..]).unwrap().1;
-    println!("{:?}", q);
+    // println!("{:?}", q);
     assert_eq!(q.queries[0].name, vec![
     msg::NameUnit::Label(String::from("nic")),
     msg::NameUnit::Label(String::from("cz")),
@@ -86,62 +90,15 @@ fn test_parse_query() {
     ]);
 }
 
-// #[test]
-// fn test_parse_query() {
-//     let input = vec![0x48, 0xe0, 0x01, 0x20, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
-//     0x03, 0x6e, 0x69, 0x63, 0x02, 0x63, 0x7a, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x29,
-//     0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
-//     let q = msg::parse_dns_query(&input[..]).unwrap().1;
-//     println!("{:?}", q);
-//     assert_eq!(q.names[0], "nic.cz")
-// }
-//
-// #[test]
-// fn parse_name_simple() {
-//     let name = "test.example.com.";
-//     let mut buffer = Vec::new();
-//     let labels: Vec<&str> = name.split_terminator('.').collect();
-//     for label in labels {
-//         let len = label.len() as u8;
-//         buffer.push(len);
-//         buffer.extend(label.as_bytes().iter().cloned());
-//     }
-//     buffer.push(0);
-//
-//     // TODO: Parse the name in wire format
-//     let labels: Vec<&str> = msg::parse_dns_name(&buffer[..]).unwrap().1;
-//     // POZOR: na konci je prazdny label
-//     let result = labels.join(".");
-//     assert_eq!(result, "test.example.com")
-// }
-//
-// #[test]
-// fn parse_name_2() {
-//     let name = "test.example.com.";
-//     let mut buffer = Vec::new();
-//     let labels: Vec<&str> = name.split_terminator('.').collect();
-//     for label in labels {
-//         let len = label.len() as u8;
-//         buffer.push(len);
-//         buffer.extend(label.as_bytes().iter().cloned());
-//     }
-//     buffer.push(0);
-//     buffer.push(5);
-//     buffer.extend(b"abcde");
-//     buffer.push(0);
-//
-//     let labels = msg::parse_dns_name(&buffer[..]).unwrap().1;
-//     let result = labels.join(".");
-//     assert_eq!(result, "test.example.com")
-// }
-//
-
-// #[test]
-// fn parse_query2() {
-//     let input = vec![0x48, 0xe0, 0x01, 0x20, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
-//     0x03, 0x6e, 0x69, 0x63, 0x02, 0x63, 0x7a, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x29,
-//     0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
-//     let q = msg::parse_dns_query2(&input[..]).unwrap();
-//     println!("{:?}", q);
-//     //assert_eq!(q.names[0], "nic.cz")
-// }
+#[test]
+fn test_parse_dns_rr_a_address() {
+    let input = vec![0xc9, 0xba, 0x81, 0xa0, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00,
+    0x01, 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00,
+    0x00, 0x01, 0x00, 0x01, 0xc0, 0x0c, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x18, 0x5c,
+    0x00, 0x04, 0x5d, 0xb8, 0xd8, 0x22, 0x00, 0x00, 0x29, 0x02, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00];
+    // From wireshark: example.com: type A, class IN, addr 93.184.216.34
+    let ref output = msg::parse_dns_message(&input[..]).unwrap().1.answer[0];
+    //println!("{:?}", output);
+    assert_eq!(output.rdata, Rdata::A(Ipv4Addr::from_str("93.184.216.34").unwrap()));
+}
